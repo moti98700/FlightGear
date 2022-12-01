cdd5de7e7af1934df833515a6b1e8ac6d395975b
moti98700 <94778544+moti98700@users.noreply.github.com>
2022-12-02 01:34:35 +0200

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -1,31 +1,88 @@
 #include "Database.hpp"
+#include "WhileCommand.hpp"
+#include "Commands.hpp"
+
+using namespace std;
 Database *Database::instance = 0;
 
 Database *Database::getInstance()
 {
-	if(!instance) {
+    if (!instance)
+    {
         instance = new Database();
     }
-	return instance;
+    return instance;
+}
+Database::Database()
+{
+    buildCommandMap();
 }
 
-void Database::setSymbolTable(string path,double value)
+void Database::setSymbolTable(string const &path, double value)
 {
     this->SymbolTable[path] = value;
 }
-void Database::setVarTable(string varName,string varPath)
+
+void Database::setVar(string const &varName, string varPath)
 {
     this->VarTable[varName] = varPath;
 }
 
+double Database::getSymbole(string const &puth)
+{
+    return SymbolTable[puth];
+}
+
+string Database::getVar(string const &var)
+{
+    return VarTable[var];
+}
+
+double Database::getVarValue(const std::string &var)
+{
+    return SymbolTable[getVar(var)];
+}
+
+Command *Database::getCommand(const std::string &command)
+{
+    return CommandMap[command];
+}
+
+bool Database::containsMap(string const &command)
+{
+    if (CommandMap.count(command))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Database::containsVar(string const &command)
+{
+    if (VarTable.count(command))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Database::containsSymbol(string const &command)
+{
+    if (SymbolTable.count(command))
+    {
+        return true;
+    }
+    return false;
+}
+
 void Database::buildCommandMap()
 {
-    CommandMap["openDataServer"] = new openServerCommand();
-    CommandMap["connect"] = new connectCommand();
-    CommandMap["var"] = new varCommand();
-    CommandMap["="] = new setCommand();
-    CommandMap["while"] = new whileCommand();
-    CommandMap["print"] = new printCommand();
-    CommandMap["sleep"] = new sleepCommand();
-    CommandMap["unknownCommand"] = new unknownCommand();
+    CommandMap["openDataServer"] = new OpenServerCommand();
+    CommandMap["connect"] = new ConnectCommand();
+    CommandMap["var"] = new VarCommand();
+    CommandMap["="] = new SetCommand();
+    CommandMap["while"] = new WhileCommand();
+    CommandMap["print"] = new PrintCommand();
+    CommandMap["sleep"] = new SleepCommand();
+    CommandMap["unknownCommand"] = new UnknownCommand();
 }
